a3ce83024fd016097173e45f0a24dea405c19e44
Thais LIns <thaisfli@hotmail.com>
2016-12-06 18:08:38 -0300

    Zeramos3.

--- TrabalhoTrem/Client/client.cpp
+++ TrabalhoTrem/Client/client.cpp
@@ -48,13 +48,13 @@ int PotentiometerSpeed() {
           ledR.setValue(BlackLib::low);
           ledG.setValue(BlackLib::low);
           ledB.setValue(BlackLib::high);
-          velocidade = 250;
+          velocidade = 500;
             }
         else if (readAnalog(1) > 1365 and readAnalog(1) <= 2720) {
           ledR.setValue(BlackLib::low);
           ledG.setValue(BlackLib::high);
           ledB.setValue(BlackLib::low);
-          velocidade = 100;
+          velocidade = 250;
             }
         else if (readAnalog(1) > 2720 and readAnalog(1) <=4095) {
           ledR.setValue(BlackLib::high);
@@ -71,6 +71,11 @@ int main(int argc, char *argv[])
 {
 
     BlackLib::BlackGPIO   led1(BlackLib::GPIO_48,BlackLib::output, BlackLib::SecureMode);
+    BlackLib::BlackGPIO   button1(BlackLib::GPIO_115,BlackLib::input); 
+    BlackLib::BlackGPIO   ledR(BlackLib::GPIO_60,BlackLib::output, BlackLib::SecureMode);
+    BlackLib::BlackGPIO   ledG(BlackLib::GPIO_50,BlackLib::output, BlackLib::SecureMode);
+    BlackLib::BlackGPIO   ledB(BlackLib::GPIO_51,BlackLib::output, BlackLib::SecureMode);
+    
     led1.setValue(BlackLib::high);
 
     struct sockaddr_in endereco;
@@ -92,6 +97,12 @@ int main(int argc, char *argv[])
         std::cin >> test.todo;
 
         switch(test.todo){
+            case 0:
+                led1.setValue(BlackLib::low);
+                ledR.setValue(BlackLib::low);
+                ledG.setValue(BlackLib::low);
+                ledB.setValue(BlackLib::low);
+                return 0;
             case 1:
                 std::cout << "Qual será a velocidade para todos os trens?" << std::endl;
                 test.a = PotentiometerSpeed();
@@ -100,75 +111,68 @@ int main(int argc, char *argv[])
                 std::cout << "Escolha um trem: ";
                 std::cin >> test.a;
                 std::cout << "Qual será a sua velocidade? " << endl;
-                test.a = PotentiometerSpeed(); 
+                test.b = PotentiometerSpeed(); 
                 break;
             case 3:
                 std::cout << "Você quer ligar ou desligar os trens?" << endl;
                 std::cin >> test.a;
+                break;
             case 4:
                 std::cout << "Escolha um trem para ligar ou desligar? " << endl;
                 std::cin >> test.a;
                 std::cout << "Você quer ligar ou desligar o trem? " << endl;
                 std::cin >> test.b;
+                break;
         default:
                 break;
-    }
-}
+        }
 
-    /*std::cout << "A: ";
-    std::cin >> test.a;
-
-    if (test.todo == 2 || test.todo == 4) {
-        std::cout << "B: ";
-        std::cin >> test.b;
-    }*/
-
-    int bytesenviados;
- 
-    /*
-     * Configurações do endereço
-    */
-    memset(&endereco, 0, sizeof(endereco));
-    endereco.sin_family = AF_INET;
-    endereco.sin_port = htons(PORTNUM);
-    endereco.sin_addr.s_addr = inet_addr("192.168.7.1"); // PUT YOUR IP HERE "ip addr show" no terminal
- 
-    /*
-     * Criando o Socket
-     *
-     * PARAM1: AF_INET ou AF_INET6 (IPV4 ou IPV6)
-     * PARAM2: SOCK_STREAM ou SOCK_DGRAM
-     * PARAM3: protocolo (IP, UDP, TCP, etc). Valor 0 escolhe automaticamente
-    */
-    socketId = socket(AF_INET, SOCK_STREAM, 0);
- 
-    //Verificar erros
-    if (socketId == -1)
-    {
-        printf("Falha ao executar socket()\n");
-        exit(EXIT_FAILURE);
+        int bytesenviados;
+     
+        /*
+         * Configurações do endereço
+        */
+        memset(&endereco, 0, sizeof(endereco));
+        endereco.sin_family = AF_INET;
+        endereco.sin_port = htons(PORTNUM);
+        endereco.sin_addr.s_addr = inet_addr("192.168.7.1"); // PUT YOUR IP HERE "ip addr show" no terminal
+     
+        /*
+         * Criando o Socket
+         *
+         * PARAM1: AF_INET ou AF_INET6 (IPV4 ou IPV6)
+         * PARAM2: SOCK_STREAM ou SOCK_DGRAM
+         * PARAM3: protocolo (IP, UDP, TCP, etc). Valor 0 escolhe automaticamente
+        */
+        socketId = socket(AF_INET, SOCK_STREAM, 0);
+     
+        //Verificar erros
+        if (socketId == -1)
+        {
+            printf("Falha ao executar socket()\n");
+            exit(EXIT_FAILURE);
+        }
+     
+        //Conectando o socket cliente ao socket servidor
+        if ( connect (socketId, (struct sockaddr *)&endereco, sizeof(struct sockaddr)) == -1 )
+        {
+            printf("Falha ao executar connect()\n");
+            exit(EXIT_FAILURE);
+        }
+        printf ("Cliente conectado ao servidor\n");
+     
+        //Enviar uma msg para o cliente que se conectou
+        printf("Cliente vai enviar uma mensagem\n");
+        bytesenviados = send(socketId,&test,sizeof(test),0);
+     
+        if (bytesenviados == -1)
+        {
+            printf("Falha ao executar send()");
+            exit(EXIT_FAILURE);
+        }
+        printf("Cliente enviou a seguinte msg (%d bytes) para o servidor: %c \n",bytesenviados,test.todo);
+     
+        close(socketId);
     }
- 
-    //Conectando o socket cliente ao socket servidor
-    if ( connect (socketId, (struct sockaddr *)&endereco, sizeof(struct sockaddr)) == -1 )
-    {
-        printf("Falha ao executar connect()\n");
-        exit(EXIT_FAILURE);
-    }
-    printf ("Cliente conectado ao servidor\n");
- 
-    //Enviar uma msg para o cliente que se conectou
-    printf("Cliente vai enviar uma mensagem\n");
-    bytesenviados = send(socketId,&test,sizeof(test),0);
- 
-    if (bytesenviados == -1)
-    {
-        printf("Falha ao executar send()");
-        exit(EXIT_FAILURE);
-    }
-    printf("Cliente enviou a seguinte msg (%d bytes) para o servidor: %c \n",bytesenviados,test.todo);
- 
-    close(socketId);
- 
     return 0;
 }
